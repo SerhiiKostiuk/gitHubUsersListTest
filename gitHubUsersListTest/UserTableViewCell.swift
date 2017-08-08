f981471dc3ebf7fe826945a0e943da9637aa6c44
Serhii Kostiuk <k.seryoga@gmail.com>
2017-08-09 00:35:05 +0300

    add table view cell

---
+++ gitHubUsersListTest/UserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserTableViewCell.swift
+//  gitHubUsersListTest
+//
+//  Created by Сергій Костюк on 8/9/17.
+//  Copyright © 2017 Serhii Kostiuk. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
