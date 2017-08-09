79fab2e37bc81e2c70b2e44e89046283e5c0a2f8
SerhiiKostiuk <serhii.kostiuk@foreway.pro>
2017-08-09 13:36:03 +0300

    add delegate func and table view data source func

--- gitHubUsersListTest/UserFollowersViewController.swift
+++ gitHubUsersListTest/UserFollowersViewController.swift
@@ -8,28 +8,41 @@
 
 import UIKit
 
-class UserFollowersViewController: UIViewController {
+class UserFollowersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var user: UserModel!
+    var userFollowers = [UserModel]()
 
+    fileprivate var apiManager = ApiManager.shared
+    @IBOutlet weak var userFollowersTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+        userFollowersTableView.register(UINib(nibName: "UserTableViewCell", bundle: nil), forCellReuseIdentifier: "UserTableViewCell")
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        apiManager.delegate = self
+        apiManager.getGitHubUsers(urlString: user.folowersUrl)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userFollowers.count
     }
-    */
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserTableViewCell") as! UserTableViewCell
+        
+        cell.fillCellWith(model: userFollowers[indexPath.item])
+        return cell
+    }
+}
 
+extension UserFollowersViewController: ApiManagerDelegate {
+    func getUserModels(array: [UserModel]!) {
+        userFollowers = array
+        
+        userFollowersTableView.reloadData()
+    }
+    
 }
