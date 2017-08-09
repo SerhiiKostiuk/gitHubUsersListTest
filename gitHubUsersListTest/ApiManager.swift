7c97d27a10a6380b703e793cfc1df87c320ffaaf
SerhiiKostiuk <serhii.kostiuk@foreway.pro>
2017-08-09 13:35:35 +0300

    refactor api manager

--- gitHubUsersListTest/ApiManager.swift
+++ gitHubUsersListTest/ApiManager.swift
@@ -8,48 +8,47 @@
 
 import Foundation
 
-protocol ApiManagerDelegate: class {
+protocol ApiManagerDelegate {
     func getUserModels(array: [UserModel]!)
 }
 
-class ApiManager: NSObject {
-//    var users: [UserModel]!
-    weak var delegate: ApiManagerDelegate?
-    
-    override init() {
-//        users = [UserModel]()
-        
-        super.init()
-    }
+struct ApiManager {
+    var delegate: ApiManagerDelegate?
+  
+    static let shared = ApiManager()
     
      func getGitHubUsers(urlString: String) {
         let url = URL.init(string: urlString)
         
-        
         let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
             DispatchQueue.main.async(execute: {
-                self.fillWith(usersData: data!)
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! NSArray
+                    print(json)
+                    
+                    var users = [UserModel]()
+                    
+                    for item in json {
+                        let user = UserModel.init(withDictionary: item as! [String : Any])
+                        
+                        
+                        if user.userId == 135 && urlString.contains("followers") == false {
+                            self.getGitHubUsers(urlString: "https://api.github.com/users?since=135")
+                        }
+                        users.append(user)
+                    }
+                    
+                    self.delegate?.getUserModels(array: users)
+                    
+                } catch {
+                    print("Could not serialise")
+                }
             })
         }
         task.resume()
     }
     
      func fillWith(usersData: Data) {
-        do {
-            let json = try JSONSerialization.jsonObject(with: usersData, options: .mutableContainers) as! NSArray
-            print(json)
-            
-             var users = [UserModel]()
-            
-            for item in json {
-                let user = UserModel.init(withDictionary: item as! [String : Any])
-                users.append(user)
-            }
-            
-            self.delegate?.getUserModels(array: users)
-            
-        } catch {
-            print("Could not serialise")
-        }
+       
     }
 }
